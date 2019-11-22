bef8defab6e01dd6c7d50021e4f49ba4e9e99501
bcheek53 <bcheek53@gmail.com>
2019-11-21 22:38:20 -0600

    adding additional filter capability

--- 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -23,17 +23,39 @@ function rowData(ufoReport) {
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
-
+  d3.event.preventDefault();
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-
+  
   var filterInputs = {};
 
   if (datetime !== "") {
       filterInputs.datetime = inputValue;
+  }
+
+  var inputState = d3.select("#state");
+  var state = inputState.property("value").toLowerCase();
+
+  if (state !== "") {
+      filterInputs.state = state;
+  }
+
+
+  var inputCity = d3.select("#city");
+  var city = inputCity.property("value").toLowerCase();
+
+  if (city !== "") {
+      filterInputs.city = city;
+  }
+
+  var inputShape = d3.select("#shape-filter");
+  var shape = inputShape.property("value").toLowerCase();
+
+  if (shape !== "") {
+      filterInputs.shape = shape;
   }
 
   var filtered = tableData.filter(obj => {
@@ -52,29 +74,13 @@ filtered.forEach(rowData);
 
 });
 
-// // Create a custom filtering function
-// function selectDate(datefilter) {
-//   return datefilter.datetime = inputValue;
-// }
+// Select the Reset button
+var buttonReset = d3.select("#reset-btn");
 
-// // filter() uses the custom function as its argument
-// var filterDate = tableData.filter(selectDate);
+buttonReset.on("click", function() {
+  d3.event.preventDefault();
+  var allFilters = d3.selectAll("input")
+  .property("value", ""); 
+  tableData.forEach(rowData);
 
-// // Test
-// console.log(filterDate);
-
-
-
-
-// // Step 1: Loop Through `data` and console.log each ufo object
-// filterDate.forEach(function(ufoReport) {
-//   console.log(ufoReport);
-// });
-
-// filterDate.forEach((ufoReport) => {
-// var row = tbody.append("tr");
-// Object.entries(ufoReport).forEach(([key, value]) => {
-//   var cell = row.append("td");
-//   cell.text(value);
-// });
-// });
+});
